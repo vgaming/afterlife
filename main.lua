99bccecb35800cec1866a6b09923c1169bfeb28d
Vasya N <n1m5spm@yandex.com>
2018-02-06 00:31:52 +0300

    fix wording in about.txt

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ local string = string
 local T = wesnoth.require("lua/helper.lua").set_wml_tag_metatable {}
 
 
-local wave_length = 2  -- also change experience_modifier in _main.cfg
+local wave_length = 2  -- also change: experience_modifier in _main.cfg, text in about.txt
 local copy_strength_start = 20
 local copy_strength_increase = 5
 
