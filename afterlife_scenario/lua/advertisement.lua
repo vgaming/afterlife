837fdd4302baa4a42110c7f2eecdcea3b2fd9c98
Vasya N <n1m5spm@yandex.com>
2018-08-05 21:16:32 +0300

    remove duplication, clean up

--- afterlife_scenario/lua/advertisement.lua
+++ afterlife_scenario/lua/advertisement.lua
@@ -1,21 +1,23 @@
--- << mirrorfaction_advertisement
+-- << afterlife_advertisement
 
 local wesnoth = wesnoth
 local tostring = tostring
 
 local script_arguments = ...
 local remote_version = tostring(script_arguments.remote_version)
+local filename = "~add-ons/afterlife_scenario/target/version.txt"
 
-if not wesnoth.have_file("~add-ons/afterlife_scenario/target/version.txt") then
+if not wesnoth.have_file(filename) then
 	wesnoth.message("Afterlife", "If you('ll) like the map, feel free to download it. Name is \"Afterlife\".")
 else
-	local local_version = wesnoth.read_file("~add-ons/afterlife_scenario/target/version.txt")
+	local local_version = wesnoth.read_file(filename)
 	if wesnoth.compare_versions(remote_version, ">", local_version) then
 		wesnoth.wml_actions.message {
 			caption = "Afterlife",
-			message = [[🠉🠉🠉 Please upgrade your Afterlife version. 🠉🠉🠉
-
-(You can do that after the game)]],
+			message = "🠉🠉🠉 Please upgrade your Afterlife version. 🠉🠉🠉"
+				.. "\n\n"
+				.. local_version .. " -> " .. remote_version
+				.. "(You can do that after the game)",
 			image = "misc/blank-hex.png~BLIT(units/human-loyalists/spearman.png~CROP(20,0,31,72)~FL())~BLIT(units/human-loyalists/spearman.png~CROP(20,0,31,72)~GS(),36,0)",
 		}
 	end
